b9c5d26321aea278338671c85a5ed6810e414755
hoseingp <mrhoseinekrami@gmail.com>
2020-08-25 11:49:27 -0400

    new chagne public url

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -5,7 +5,7 @@ import * as CSS from './css/Card.module.scss';
 const btnTitle = "Book It";
 const Cardainer = ({ name, desc, img }) => {
     return <Card className={CSS.card}>
-        <Card.Img variant="top" src={img ? require(process.env.PUBLIC_URL + `/imgs/hotels/${img}`) : "holder.js/100px180"} />
+        <Card.Img variant="top" src={img ? process.env.PUBLIC_URL + `/imgs/hotels/${img}` : "holder.js/100px180"} />
         <Card.Body>
             <Card.Title>{name}</Card.Title>
             <Card.Text>
